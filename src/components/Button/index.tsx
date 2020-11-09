18c239cd112b8c298c35af6700e0f4fc6a41782f
Raims Cabrera <raimscabrera@gmail.com>
2020-11-09 15:54:09 +0800

    Adds type prop to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,18 +3,20 @@ import React, { Component } from 'react'
 interface ButtonProps {
   label: string
   handleClick: VoidFunction
-  icon: Component | null
+  icon: any // help how do I interface an HTML element LOL
   className: string
+  type: 'submit' | 'button' | 'reset'
 }
 
 const Button = ({
   label = 'Button',
   handleClick = () => {},
   icon = null,
-  className = ''
+  className = '',
+  type = 'button'
 }: ButtonProps) => {
   return (
-    <button className={className} onClick={handleClick}>
+    <button className={className} onClick={handleClick} type={type}>
       {icon}
       {label}
     </button>

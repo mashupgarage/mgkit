4faa31c402537cb36c771e716fe5ca6ff53f48e8
Raims Cabrera <raimscabrera@gmail.com>
2020-11-23 11:04:43 +0800

    Adds vertical field variant

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -4,19 +4,42 @@ import styled from 'styled-components'
 export interface FieldProps {
   label?: string
   type?: 'text' | 'email' | 'number'
+  vertical?: boolean
 }
 
-const Container = styled.div`
+type ContainerProps = {
+  vertical: boolean
+}
+
+const Container = styled.div<ContainerProps>`
   font-family: sans-serif;
+  ${({ vertical }) => {
+    if (vertical) {
+      return `
+        display: flex;
+        flex-direction: column;
+      `
+    }
+  }}
 
   > label {
-    margin-right: 8px;
+    ${({ vertical }) => {
+      if (vertical) {
+        return `
+          margin-bottom: 8px;
+        `
+      }
+
+      return `
+        margin-right: 8px;
+      `
+    }}
     font-weight: bold;
   }
 
   > input {
     padding: 8px;
-    border: 2px solid lightgrey; 
+    border: 2px solid lightgrey;
     border-radius: 4px;
     transition: 250ms ease-out;
   }
@@ -32,9 +55,13 @@ const Container = styled.div`
   }
 `
 
-export const Field:FC<FieldProps> = ({ label = 'Label', type = 'text' }) => {
+export const Field: FC<FieldProps> = ({
+  label = 'Label',
+  type = 'text',
+  vertical = false
+}) => {
   return (
-    <Container>
+    <Container vertical={vertical}>
       <label>{label}</label>
       <input type={type} />
     </Container>

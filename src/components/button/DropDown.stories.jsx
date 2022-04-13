22287341fb202c42e040c6994c4eb8c84458975b
Kevin Samala <k.samala034@gmail.com>
2022-04-13 17:09:34 +0800

    Adds more variants to Drop downs

--- src/components/button/DropDown.stories.jsx
+++ src/components/button/DropDown.stories.jsx
@@ -8,4 +8,6 @@ export default {
 };
 
 export const Primary = () => <DropDown style="kit-dropdown"/>;
+export const PrimaryLight = () => <DropDown style="kit-dropdown -light"/>;
+export const PrimaryDisabled = () => <DropDown style="kit-dropdown -disabled"/>;
 export const PrimaryLineless = () => <DropDown style="kit-dropdown -lineless"/>;

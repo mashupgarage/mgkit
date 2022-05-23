292ae60d4e9e69e6e2d7b895832aaaac19f27118
Kevin Samala <k.samala034@gmail.com>
2022-05-23 17:10:51 +0800

    Fixes Indent syntax

--- src/components/button/TopNavbar.jsx
+++ src/components/button/TopNavbar.jsx
@@ -5,18 +5,18 @@ import MenuIcon from '../../assets/MenuIcon.svg';
 
 export function TopNavbar({ style }) {
   return (
-    <div class={style}> 
-			<img class="monogram -primary" src={MonogramWhite} alt="logo"/>
-      <img class="monogram -light" src={MonogramBlack} alt="logo"/>
-			<div class="links">
-				<span> <img class="menu" src={MenuIcon} alt="Menu"/></span>
+    <div class={style}>
+      <img class="monogram -primary" src={MonogramWhite} alt="logo" />
+      <img class="monogram -light" src={MonogramBlack} alt="logo" />
+      <div class="links">
+        <span><img class="menu" src={MenuIcon} alt="Menu" /></span>
         <a href="#">Menu</a>
-				<a href="#">Menu</a>
-				<a href="#">Menu</a>
-				<a href="#">Menu</a>
-				<a href="#">Menu</a>
-			  <div class="profile"></div>
-		  </div>
+        <a href="#">Menu</a>
+        <a href="#">Menu</a>
+        <a href="#">Menu</a>
+        <a href="#">Menu</a>
+        <div class="profile"></div>
+      </div>
     </div>
   );
 }
